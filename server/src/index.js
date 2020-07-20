7942467547a1f787ed8f26a862d6632041131b38
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-20 09:42:12 +0200

    setup origin for front-end

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 
 const PORT = process.env.PORT || 5000;
 
