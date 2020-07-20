17c022a2b66238604f5e4629c330726310d41b8a
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-20 09:32:25 +0200

    added semicolons

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Listening at ${PORT}`)
-})
+  console.log(`Listening at ${PORT}`);
+});
