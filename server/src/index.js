810d8844a82aa2a25a804d50b192b606e8d3c285
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-21 23:03:33 +0200

    imported mongoose

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const middlewares = require('./middlewares');
 
