b34ef9a56f9b082bee05c1626a9fa98d54f20a70
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-21 22:41:01 +0200

    declared helper variable of requiredString

--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -2,4 +2,12 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const logEntrySchema = new Schema({});
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const logEntrySchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+});
