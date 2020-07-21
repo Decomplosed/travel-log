e0d8c9466a5b37bd36f414eba6f1125b094921c8
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-21 22:31:39 +0200

    moved errorHandler middleware

--- server/src/index.js
+++ server/src/index.js
@@ -21,14 +21,7 @@ app.get('/', (req, res) => {
 app.use();
 
 // eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
-  });
-});
+app.use();
 
 const PORT = process.env.PORT || 5000;
 
