33d76aa7f8e54eda54c10dddc372473920da7033
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-22 23:16:22 +0200

    created a root route

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -2,4 +2,10 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.json({
+    message: 'root route',
+  });
+});
+
 module.exports = router;
