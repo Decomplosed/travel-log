3aba2d2d10f910f2403641c354acea43a0af589a
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-24 19:51:08 +0200

    made post route to be adsync

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const logEntry = new LogEntry(req.body);
   console.log(req.body);
 });
