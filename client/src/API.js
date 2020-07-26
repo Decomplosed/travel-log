86d5ba07e4e34874a248ab0a96e98a785eaba99d
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-26 23:01:55 +0200

    exported out listLogEntries

--- client/src/API.js
+++ client/src/API.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://localhost:5000';
 
-async function listLogEntries() {
-  const response = await fetch(`${API_URL}/api/logs`)
-  return response.json()
+export async function listLogEntries() {
+  const response = await fetch(`${API_URL}/api/logs`);
+  return response.json();
 }
