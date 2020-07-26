7b296df31bd2964aad408fb9371357da84f37a81
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-26 22:41:56 +0200

    changed with and height of map to 100%

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import ReactMapGL from 'react-map-gl';
 
 const App = () => {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+    width: '100%',
+    height: '100%',
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8,
