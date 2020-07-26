4f19f649bdfd1e472b38073b396948819cbc678f
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-26 22:40:14 +0200

    fied typo

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import ReactMapGL from 'react-map-gl';
 const App = () => {
   const [viewport, setViewport] = useState({
     width: 400,
-    heigh: 400,
+    height: 400,
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8,
