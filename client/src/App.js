f42af946eaa1ded0d005ffdcce07ffda4f80179b
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-26 22:44:20 +0200

    turned % to vh and vw for map

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import ReactMapGL from 'react-map-gl';
 
 const App = () => {
   const [viewport, setViewport] = useState({
-    width: '100%',
-    height: '100%',
+    width: '100vw',
+    height: '100vh',
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8,
