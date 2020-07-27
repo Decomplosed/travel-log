c6b9d4e891c1de4a0281c60a93ef701e6b05fa11
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-27 23:38:13 +0200

    called setLogEntries inside useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ const App = () => {
   useEffect(() => {
     (async () => {
       const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
       console.log(logEntries);
     })();
   }, []);
