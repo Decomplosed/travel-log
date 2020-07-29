7cf704047393bccbf1a7b4d713e4b9385ecee5e3
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-29 18:28:52 +0200

    wrapped svg inside div container

--- client/src/App.js
+++ client/src/App.js
@@ -37,22 +37,24 @@ const App = () => {
           offsetLeft={-20}
           offsetTop={-10}
         >
-          <svg
-            className='marker'
-            style={{
-              width: '24px',
-              height: '24px',
-            }}
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='#179ad9'
-            stroke-width='1.5'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-          >
-            <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'></path>
-            <circle cx='12' cy='10' r='3'></circle>
-          </svg>
+          <div>
+            <svg
+              className='marker'
+              style={{
+                width: '24px',
+                height: '24px',
+              }}
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='#179ad9'
+              stroke-width='1.5'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            >
+              <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'></path>
+              <circle cx='12' cy='10' r='3'></circle>
+            </svg>
+          </div>
         </Marker>
       ))}
     </ReactMapGL>
