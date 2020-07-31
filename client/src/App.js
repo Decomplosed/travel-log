979f14fd78be9dfc6a953ed370c4b53e148f0a29
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-31 23:25:01 +0200

    changed svg to img

--- client/src/App.js
+++ client/src/App.js
@@ -38,22 +38,7 @@ const App = () => {
           offsetTop={-10}
         >
           <div>
-            <svg
-              className='marker'
-              style={{
-                width: `calc(1vmin * ${viewport.zoom})`,
-                height: `calc(1vmin * ${viewport.zoom})`,
-              }}
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='#179ad9'
-              stroke-width='1.5'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-            >
-              <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'></path>
-              <circle cx='12' cy='10' r='3'></circle>
-            </svg>
+            <img className='marker' src={pinIcon} alt='pin' />
           </div>
         </Marker>
       ))}
