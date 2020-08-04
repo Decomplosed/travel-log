7b69bb6dc8ae20751e5539f3ed4a3e8e75a0ab35
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-04 15:12:17 +0200

    replaced code with new icon

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
 import { listLogEntries } from './API';
 
-import pinIcon from './icons/pin.jpeg';
+import pinIcon from './icons/pin-icon.png';
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
