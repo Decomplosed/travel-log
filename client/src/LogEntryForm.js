7bd417c4465238420db8e1447aaa13e40d6f3c3e
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-04 15:32:06 +0200

    added comments and description inputs and textareas

--- client/src/LogEntryForm.js
+++ client/src/LogEntryForm.js
@@ -5,6 +5,10 @@ const LogEntryForm = () => {
     <form>
       <label htmlFor='title'>Title</label>
       <input name='title' />
+      <label htmlFor='comments'>Comments</label>
+      <textarea name='comments' rows={3} />
+      <label htmlFor='description'>Description</label>
+      <textarea name='description' rows={3} />
     </form>
   );
 };
