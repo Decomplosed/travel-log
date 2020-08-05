8d81fc12e4dd8b54d81bb7aeb81f631b022a8d64
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-05 16:22:47 +0200

    register ref added to inputs and trextareas

--- client/src/LogEntryForm.js
+++ client/src/LogEntryForm.js
@@ -6,15 +6,15 @@ const LogEntryForm = () => {
   return (
     <form className='entry-form'>
       <label htmlFor='title'>Title</label>
-      <input name='title' required />
+      <input name='title' required ref={register} />
       <label htmlFor='comments'>Comments</label>
-      <textarea name='comments' rows={3} />
+      <textarea name='comments' rows={3} ref={register} />
       <label htmlFor='description'>Description</label>
-      <textarea name='description' rows={3} />
+      <textarea name='description' rows={3} ref={register} />
       <label htmlFor='image'>Image</label>
-      <input name='image' />
+      <input name='image' ref={register} />
       <label htmlFor='visitDate'>Visit Date</label>
-      <input name='visitDate' type='date' required />
+      <input name='visitDate' type='date' required ref={register} />
       <button>Create Travel Log</button>
     </form>
   );
