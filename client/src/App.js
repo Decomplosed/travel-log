78ea014fa22eee09b3ad34617fa86aac11aac287
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-05 16:35:35 +0200

    passed location prop to LogEntryForm

--- client/src/App.js
+++ client/src/App.js
@@ -135,7 +135,7 @@ const App = () => {
             onClose={() => setAddEntryLocation(null)}
           >
             <div className='popup'>
-              <LogEntryForm />
+              <LogEntryForm location={addEntryLocation} />
             </div>
           </Popup>
         </>
